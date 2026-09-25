cbe92d9157ad80c081614cb9f1f552527a438804
Maham <mahamshahid80@gmail.com>
2026-09-25 13:08:47 +0500

    Import App.css

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from "react-router-dom";
 
 import { useEffect, useState } from "react";
+import "./App.css";
 
 import ManageMenu from "./ManageMenu";
 import ManageReservations from "./ManageReservations";
